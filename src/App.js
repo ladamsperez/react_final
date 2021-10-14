ec42b350358f522bdf308827a548d09179d54be6
ladamsperez <ladamsperezwork@gmail.com>
2021-10-14 11:23:48 -0400

    began structuring components and renders for drf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AddE1337 from "./components/AddE1337";
+import E1337 from "./components/E1337";
+import E1337List from "./components/E1337List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/e1337" className="navbar-brand">
+          bezKoder
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/e1337"} className="nav-link">
+              Tutorials
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/tutorials"]} component={E1337List} />
+          <Route exact path="/add" component={AddE1337} />
+          <Route path="/e1337/:id" component={E1337} />
+        </Switch>
+      </div>
     </div>
   );
 }
+
+
+// class App extends Component {
+//   render() {
+//     // ...
+//   }
+// }
 
 export default App;
